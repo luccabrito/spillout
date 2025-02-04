ade16f6d677595f4846684fabc3876acb8a3121d
Lucca Brito <luccabritogt@gmail.com>
2025-02-04 17:39:29 -0300

    Adjusted ktlint + build.gradle.

--- src/main/kotlin/bsky/bot/spillout/controller/LyricsController.kt
+++ src/main/kotlin/bsky/bot/spillout/controller/LyricsController.kt
@@ -3,11 +3,17 @@ package bsky.bot.spillout.controller
 import bsky.bot.spillout.model.Lyrics
 import bsky.bot.spillout.service.LyricsService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/lyrics")
-@CrossOrigin(origins = ["http://localhost:5500"])
+@SuppressWarnings("WildcardImport")
 class LyricsController(
     private val service: LyricsService
 ) {
